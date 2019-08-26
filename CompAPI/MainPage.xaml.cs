1b89758d8f04224c1a9e796cf58672596b88cc17
haopeng <haopeng__@outlook.com>
2019-08-26 16:16:52 +0800

    1

--- CompAPI/MainPage.xaml.cs
+++ CompAPI/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using CompAPI.Model;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +28,42 @@ namespace CompAPI
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Movie> Movies;
         public MainPage()
         {
             this.InitializeComponent();
+
+            Movies = new ObservableCollection<Movie>();
+            xamlMovies.ItemsSource = Movies;
+
+            Loaded += MainPage_Loaded;
         }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadPage(1);
+            await LoadPage(2);
+            await LoadPage(3);
+            await LoadPage(4);
+        }
+
+        private async Task LoadPage(int page)
+        {
+            var movies = await GetPage(page);
+            foreach (var movie in movies)
+            {
+                Movies.Add(movie);
+            }
+        }
+
+        private async Task<List<Movie>> GetPage(int page)
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/data/page{page}.json"));
+            var str = await FileIO.ReadTextAsync(file);
+            var obj = JsonConvert.DeserializeObject<MovieData>(str);
+
+            return obj.content.Select(Movie.From).ToList();
+        }
+
     }
 }
---
+++ CompAPI/Model/Movie.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompAPI.Model
+{
+    public class Movie
+    {
+        public string Img { get; set; }
+
+        public static Movie From(Content data)
+        {
+            return new Movie()
+            {
+                Img = data.image_url
+            };
+        }
+    }
+
+    public class MovieData
+    {
+        public Content[] content { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Content
+    {
+        public string[] actor { get; set; }
+        public string[] age { get; set; }
+        public string brief { get; set; }
+        public object[] category { get; set; }
+        public int comment_count { get; set; }
+        public object[] content { get; set; }
+        public int content_type { get; set; }
+        public string[] creator { get; set; }
+        public string description { get; set; }
+        public string display_name { get; set; }
+        public object[] dubber { get; set; }
+        public object[] duration { get; set; }
+        public long entity_id { get; set; }
+        public string entity_type { get; set; }
+        public int format_duration { get; set; }
+        public object[] guest { get; set; }
+        public object[] host { get; set; }
+        public string image_url { get; set; }
+        public object[] industry { get; set; }
+        public int is_end { get; set; }
+        public string[] language { get; set; }
+        public int latest_episode { get; set; }
+        public int mark_type { get; set; }
+        public object[] movie_type { get; set; }
+        public object[] music_type { get; set; }
+        public object[] new_type { get; set; }
+        public int online_count { get; set; }
+        public int pay_mark { get; set; }
+        public string[] place { get; set; }
+        public int play_count { get; set; }
+        public object[] property { get; set; }
+        public string publish_date { get; set; }
+        public string[] school { get; set; }
+        public float score { get; set; }
+        public object[] season { get; set; }
+        public string[] specification { get; set; }
+        public object[] sport { get; set; }
+        public string[] style { get; set; }
+        public string sub_title { get; set; }
+        public int total_episode { get; set; }
+        public string[] type { get; set; }
+        public long update_time { get; set; }
+        public string update_tip { get; set; }
+        public string uploader_name { get; set; }
+        public object[] version { get; set; }
+        public object[] video_type { get; set; }
+        public int year { get; set; }
+    }
+}
